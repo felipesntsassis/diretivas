c0ad3e33a07ee238cc035976faac7129e2157723
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2024-02-22 15:20:23 -0300

    Desenvolvimento das diretivas de atributo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { CompAtributosComponent } from './comp-atributos/comp-atributos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CompAtributosComponent
   ],
   imports: [
     BrowserModule,
+		FormsModule,
     AppRoutingModule
   ],
   providers: [],
